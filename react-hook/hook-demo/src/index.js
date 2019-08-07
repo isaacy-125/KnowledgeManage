1df0370f728409fad3c3d793d963f2087ddcb2ac
isaac <273070111@qq.com>
2019-08-07 11:35:03 +0800

    [fix]增加react 新特性demo

--- react-hook/hook-demo/src/index.js
+++ react-hook/hook-demo/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-import App from './App';
-import UseState from './API/UseState';
+// import App from './App';
+// import UseState from './API/UseState';
 import UseEffect from './API/UseEffect';
 import UseContext from './API/UseContext';
 import UseReducer from './API/UseReducer';
@@ -13,6 +13,16 @@ import UseRef from './API/UseRef';
 import MobxIndex from './Mobx';
 import Memoized from './Memoized';
 import * as serviceWorker from './serviceWorker';
+
+const App = React.lazy(() => new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(
+            import('./App.js')
+        );
+    }, 1000)
+}));
+
+const UseState = React.lazy(() => import('./API/UseState'));
 
 ReactDOM.render((
     <div>
@@ -28,20 +38,22 @@ ReactDOM.render((
             <li><a href="#/mobx">Mobx</a></li>
             <li><a href="#/memoized">Memoized</a></li>
         </ul>
-        <HashRouter>
-            <Switch>
-                <Route exact path="/" component={App}></Route>
-                <Route exact path="/useState" component={UseState}></Route>
-                <Route exact path="/useEffect" component={UseEffect}></Route>
-                <Route exact path="/useContext" component={UseContext}></Route>
-                <Route exact path="/useReducer" component={UseReducer}></Route>
-                <Route exact path="/useCallback" component={UseCallback}></Route>
-                <Route exact path="/useMemo" component={UseMemo}></Route>
-                <Route exact path="/useRef" component={UseRef}></Route>
-                <Route exact path="/mobx" component={MobxIndex}></Route>
-                <Route exact path="/memoized" component={Memoized}></Route>
-            </Switch>
-        </HashRouter>
+        <React.Suspense fallback={<div>Loading...</div>}>
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/" component={App}></Route>
+                    <Route exact path="/useState" component={UseState}></Route>
+                    <Route exact path="/useEffect" component={UseEffect}></Route>
+                    <Route exact path="/useContext" component={UseContext}></Route>
+                    <Route exact path="/useReducer" component={UseReducer}></Route>
+                    <Route exact path="/useCallback" component={UseCallback}></Route>
+                    <Route exact path="/useMemo" component={UseMemo}></Route>
+                    <Route exact path="/useRef" component={UseRef}></Route>
+                    <Route exact path="/mobx" component={MobxIndex}></Route>
+                    <Route exact path="/memoized" component={Memoized}></Route>
+                </Switch>
+            </HashRouter>
+        </React.Suspense>
     </div>
 ), document.getElementById('root'));
 
