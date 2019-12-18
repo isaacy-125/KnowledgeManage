d8a003aa2719383e4f2a9f67bff16dbb60ba7cfc
isaac <273070111@qq.com>
2019-12-18 17:26:27 +0800

    [ADD]增加svelte demo

--- Svelte/svelte-projects/my-svelte-project/src/Bind.svelte
+++ Svelte/svelte-projects/my-svelte-project/src/Bind.svelte
@@ -1,8 +1,33 @@
 <script>
     let name = 'world';
+    let checked = false;
+    let thisDiv;
+
+    function getWidth() {
+        console.log(thisDiv);
+    }
 </script>
 
 <!-- 双向绑定数据 -->
 <input bind:value={name} />
 
 <h1>Hello {name}</h1>
+
+<input type="checkbox" bind:checked={checked}>
+{#if checked}
+yes
+{:else}
+no
+{/if}
+
+<!-- bind:this可以获取当前ref -->
+<button bind:this={thisDiv} class="bindThis" on:click={getWidth}>
+    get this width
+</button>
+
+<style>
+    .bindThis {
+        width: 100px;
+        height: 100px;
+    }
+</style>
